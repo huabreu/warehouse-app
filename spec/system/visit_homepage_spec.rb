b929dcd7a9fb354dad68f84947ea49acf3de9d47
huabreu <hughra7@gmail.com>
2021-12-08 09:10:46 -0300

    view warehouses on front page

--- spec/system/visit_homepage_spec.rb
+++ spec/system/visit_homepage_spec.rb
@@ -4,7 +4,30 @@ describe 'visitor view homepage' do
   it 'view welcome message' do
     visit root_path
 
-    expect(page).to have_content 'Bem-vindo ao Warehouse App'
-    
+    expect(page).to have_css 'h1', text: 'Bem-vindo ao Warehouse App'
+  end
+
+  it 'view registred warehouses meassage' do
+    visit root_path
+
+    expect(page).to have_css 'h2', text: 'Galpões Cadastrados'
+  end
+
+  it 'view registered warehouses' do
+    Warehouse.new(name: 'Guarulhos', code: 'GRU').save()
+    Warehouse.new(name: 'Porto Alegre', code: 'POA').save()
+    Warehouse.new(name: 'São Luís', code: 'SLZ').save()
+    Warehouse.new(name: 'Vitória', code: 'VIX').save()
+
+    visit root_path
+
+    expect(page).to have_content 'Guarulhos'
+    expect(page).to have_content 'GRU'
+    expect(page).to have_content 'Porto Alegre'
+    expect(page).to have_content 'POA'
+    expect(page).to have_content 'São Luís'
+    expect(page).to have_content 'SLZ'
+    expect(page).to have_content 'Vitória'
+    expect(page).to have_content 'VIX'
   end
 end
