2fcf4dc482438af2074870fd864e6a4f743655a0
huabreu <hughra7@gmail.com>
2021-12-09 09:34:20 -0300

    visitor view warehouse details

---
+++ spec/system/visit_warehouse_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe 'visitor view a warehouse' do
+  it 'and see all registered data' do
+    #arrange
+    Warehouse.create(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão no centro da cidade',
+                     address: 'Av. Fernandes Lima', zip_code: '57050-000', 
+                     city: 'Maceió', state: 'AL', 
+                     total_area: 10000, useful_area: 8000)
+    #act
+    visit root_path
+    click_on 'Maceió'
+    #assert
+    expect(page).to have_content('Maceió')
+    expect(page).to have_content('MCZ')
+    expect(page).to have_content('Ótimo galpão no centro da cidade')
+    expect(page).to have_content('Av. Fernandes Lima')
+    expect(page).to have_content('CEP: 57050-000')
+    expect(page).to have_content('Maceió/AL')
+    expect(page).to have_content('Área Total: 10000 m2')
+    expect(page).to have_content('Área Útil: 8000 m2')
+  end
+end
