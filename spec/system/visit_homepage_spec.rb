509d10614021fda7fa04cba5583ed109fd0a6ef8
huabreu <hughra7@gmail.com>
2021-12-09 08:00:55 -0300

    updated to match issues

--- spec/system/visit_homepage_spec.rb
+++ spec/system/visit_homepage_spec.rb
@@ -4,13 +4,8 @@ describe 'visitor view homepage' do
   it 'view welcome message' do
     visit root_path
 
-    expect(page).to have_css 'h1', text: 'Bem-vindo ao Warehouse App'
-  end
-
-  it 'view registred warehouses meassage' do
-    visit root_path
-
-    expect(page).to have_css 'h2', text: 'Galpões Cadastrados'
+    expect(page).to have_css('h1', text: 'WareHouse App')
+    expect(page).to have_css('h3', text: 'Boas vindas ao sistema de gestão de estoques')
   end
 
   it 'view registered warehouses' do
@@ -21,6 +16,7 @@ describe 'visitor view homepage' do
 
     visit root_path
 
+    expect(page).to have_css 'h2', text: 'Galpões Cadastrados'
     expect(page).to have_content 'Guarulhos'
     expect(page).to have_content 'GRU'
     expect(page).to have_content 'Porto Alegre'
