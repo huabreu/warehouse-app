1e4c05442edee72b8a93a8b36a7b8dda9d79ecfe
huabreu <hughra7@gmail.com>
2021-12-13 23:49:43 -0300

    add uniqueness and format tests

--- app/models/warehouse.rb
+++ app/models/warehouse.rb
@@ -2,5 +2,6 @@ class Warehouse < ApplicationRecord
   validates :name, :code, :description, :address, :city, :state, :zip_code,
             :total_area, :useful_area,
   presence: true
+  validates :name, :code, uniqueness: true 
 
 end
--- spec/models/warehouse_spec.rb
+++ spec/models/warehouse_spec.rb
@@ -1,5 +1,28 @@
 require 'rails_helper'
 
 RSpec.describe Warehouse, type: :model do
-  
+
+  context 'some attributes must be unique' do
+    it 'duplicate code' do
+      Warehouse.create(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão numa linda cidade',
+      address: 'Av Fernandes Lima', city: 'Maceió', state: 'AL', zip_code: '57050-000',
+      total_area: 10000, useful_area: 8000)
+      warehouse = Warehouse.new(name: 'Curitiba', code: 'MCZ', description: 'Ótimo galpão mas é frio',
+      address: 'Av Coritiba', city: 'Curitiba', state: 'PR', zip_code: '87050-000',
+      total_area: 5000, useful_area: 4000)
+
+      expect(warehouse).not_to be_valid
+    end
+
+    it 'duplicate name' do
+      Warehouse.create(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão numa linda cidade',
+      address: 'Av Fernandes Lima', city: 'Maceió', state: 'AL', zip_code: '57050-000',
+      total_area: 10000, useful_area: 8000)
+      warehouse = Warehouse.new(name: 'Maceió', code: 'CTB', description: 'Ótimo galpão mas é frio',
+      address: 'Av Coritiba', city: 'Curitiba', state: 'PR', zip_code: '87050-000',
+      total_area: 5000, useful_area: 4000)
+
+      expect(warehouse).not_to be_valid
+    end
+  end
 end
