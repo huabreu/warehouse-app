ece327e168a1fb85596a6a5f5c99e55e83d3b6ae
huabreu <hughra7@gmail.com>
2021-12-16 08:56:33 -0300

    Add unitary tests to verify if integers fields are greater than zero

--- app/models/product_model.rb
+++ app/models/product_model.rb
@@ -3,6 +3,8 @@ class ProductModel < ApplicationRecord
 
   validates :name, :sku_code, :weight, :height, :width, :length, 
   presence: true
+  validates :weight, :height, :width, :length, 
+  numericality: { :greater_than => 0 }
 
   def dimensions()
     "#{height} x #{width} x #{length}"
--- spec/models/product_model_spec.rb
+++ spec/models/product_model_spec.rb
@@ -65,4 +65,84 @@ RSpec.describe ProductModel, type: :model do
       expect(product).not_to be_valid
     end
   end
+
+  context 'Weight must be greater than zero' do
+    it 'Weight is negative' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: -200, width: 30, height: 5, length: 12)
+
+      expect(product).not_to be_valid
+    end
+
+    it 'Weight is zero' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: 0, width: 30, height: 5, length: 12)
+
+      expect(product).not_to be_valid
+    end
+  end
+
+  context 'Width must be greater than zero' do
+    it 'Width is negative' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: 200, width: -30, height: 5, length: 12)
+
+      expect(product).not_to be_valid
+    end
+
+    it 'Width is zero' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: 200, width: 0, height: 5, length: 12)
+
+      expect(product).not_to be_valid
+    end
+  end
+
+  context 'Height must be greater than zero' do
+    it 'Height is negative' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: 200, width: 30, height: -5, length: 12)
+
+      expect(product).not_to be_valid
+    end
+
+    it 'Height is zero' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: 200, width: 30, height: 0, length: 12)
+
+      expect(product).not_to be_valid
+    end
+  end
+
+  context 'Length must be greater than zero' do
+    it 'Length is negative' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: 200, width: 30, height: 5, length: -12)
+
+      expect(product).not_to be_valid
+    end
+
+    it 'Length is zero' do
+      supplier = Supplier.create(trade_name: 'Ipiranga Logisticas', company_name: 'Ipiranga Logisticas SA', 
+                                  cnpj: '1234567891234', email: 'iippiiranga@hotmail.com')
+      product = ProductModel.new(name:'Teclado Digitador', supplier: supplier, sku_code: 'TCDD2123212322ASD2313',
+                                  weight: 200, width: 30, height: 5, length: 0)
+
+      expect(product).not_to be_valid
+    end
+  end
 end
