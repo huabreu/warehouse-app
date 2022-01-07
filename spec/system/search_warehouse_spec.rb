962a8504ed1714a6f436d12a3be99a8f1ef48cdf
huabreu <hughra7@gmail.com>
2022-01-07 09:32:20 -0300

    Add search warehouses feature

--- spec/system/search_warehouse_spec.rb
+++ spec/system/search_warehouse_spec.rb
@@ -6,7 +6,7 @@ describe 'Visitor searchs warehouse' do
       Warehouse.create!(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão no centro da cidade',
       address: 'Av. Fernandes Lima', zip_code: '57050-000', 
       city: 'Maceió', state: 'AL', total_area: 10000, useful_area: 8000)
-      Warehouse.create(name: 'Lomaco', code: 'LMC', description: 'Ótimo galpão a beira mar',
+      Warehouse.create!(name: 'Lomaco', code: 'LMC', description: 'Ótimo galpão a beira mar',
       address: 'Av. Rio Branco', zip_code: '36050-000', 
       city: 'Juiz de Fora', state: 'MG', total_area: 20000, useful_area: 13000)
       Warehouse.create!(name: 'Curitiba', code: 'CTB', description: 'Ótimo galpão mas é frio',
@@ -14,7 +14,7 @@ describe 'Visitor searchs warehouse' do
       total_area: 5000, useful_area: 4000)
 
       visit root_path
-      fill_in 'Busca', with: 'mac'
+      fill_in 'query', with: 'mac'
       click_on 'Pesquisar'
 
       expect(current_path).to eq search_warehouses_path
@@ -26,6 +26,27 @@ describe 'Visitor searchs warehouse' do
       expect(page).not_to have_content('Curitiba')
       expect(page).not_to have_content('CTB')
       expect(page).not_to have_content('PR')
+    end
+
+    it 'without success' do 
+      Warehouse.create!(name: 'Maceió', code: 'MCZ', description: 'Ótimo galpão no centro da cidade',
+      address: 'Av. Fernandes Lima', zip_code: '57050-000', 
+      city: 'Maceió', state: 'AL', total_area: 10000, useful_area: 8000)
+      Warehouse.create!(name: 'Lomaco', code: 'LMC', description: 'Ótimo galpão a beira mar',
+      address: 'Av. Rio Branco', zip_code: '36050-000', 
+      city: 'Juiz de Fora', state: 'MG', total_area: 20000, useful_area: 13000)
+
+      visit root_path
+      fill_in 'query', with: 'zu'
+      click_on 'Pesquisar'
+
+      expect(current_path).to eq search_warehouses_path
+      expect(page).to have_css('h1', text: 'Galpões Encontrados')
+      expect(page).to have_content('Não existem galpões cadastrados correspondentes. Cadastre um novo galpão ou busque por outros termos.')
+      expect(page).not_to have_content('Maceió (MCZ)')
+      expect(page).not_to have_content('Localização: Maceió - AL')
+      expect(page).not_to have_content('Lomaco (LMC)')
+      expect(page).not_to have_content('Localização: Juiz de Fora - MG')
     end
   end
 end
